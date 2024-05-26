aeb7d3686887d873a2bb1ad4471f0ee2cf05430c
ibby <ibbbyy@proton.me>
2024-05-26 13:43:06 -0700

    Added noise filtering parameter

--- effects/multiply.py
+++ effects/multiply.py
@@ -3,18 +3,25 @@ params = {
     "multiple":
         {
             "type": float,
-            "min": -5.0,  # You could go beyond these limits, but the output comes out much noisier
-            "max": 5.0,
+            "min": 1.0,
+            "max": 10.0,
+        },
+    "noisy":
+        {
+            "type": bool,
         },
     }
 
 def effect(pixeldata, **kwargs):
     multiple = kwargs["multiple"];
+    noisy = kwargs["noisy"];
 
     for byte_index in range( len(pixeldata) ):
         byte = pixeldata[byte_index];
         multiplied_byte = round(byte * multiple);
-        result_byte = multiplied_byte % 256;
-        pixeldata[byte_index] = result_byte;
+        
+        if noisy or multiplied_byte <= 255:
+            result_byte = multiplied_byte % 256;
+            pixeldata[byte_index] = result_byte;
 
     return pixeldata;
